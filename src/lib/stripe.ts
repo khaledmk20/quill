e77adffbe75498fc7150cfc07c407942ae90b66d
Khaled Abdelalim <khaled.korany2004@gmail.com>
2023-12-24 23:23:11 +0300

    stripe

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -4,7 +4,7 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
-  apiVersion: "2023-08-16", // check
+  apiVersion: "2023-08-16",
   typescript: true,
 });
 
@@ -23,7 +23,7 @@ export async function getUserSubscriptionPlan() {
 
   const dbUser = await db.user.findFirst({
     where: {
-      id: user.id,
+      id: user?.id,
     },
   });
 
